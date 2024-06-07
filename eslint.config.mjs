7dfe9e5efd0f838d88c36f4a3195959431508ee4
annysen <annysen95@gmail.com>
2024-06-07 16:38:10 +0600

    first setup done

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    files: ["**/*.js"],
+    ignores: ["**/node_modules/", ".dist/"],
+    languageOptions: { sourceType: "script" },
+    // rules
+    rules: {
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
+    },
+  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
